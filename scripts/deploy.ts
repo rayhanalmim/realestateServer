889f643a5483327c277eaac1c2f8151c9f5f12f1
rayhanalmim <rayhanalmim1@gmail.com>
2025-03-25 14:04:39 +0600

    first commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,39 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const GOVERNMENT_ACCOUNT_ADDRESS =
+    "0xEF4Eef3FFdA3E05D347986862726a955353A2227";
+
+  // We get the contract to deploy
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(GOVERNMENT_ACCOUNT_ADDRESS);
+
+  await marketplace.deployed();
+
+  console.log("Marketplace deployed: ", marketplace.address);
+
+  const PropertyNFT = await ethers.getContractFactory("PropertyNFT");
+  const nft = await PropertyNFT.deploy(marketplace.address);
+  await nft.deployed();
+
+  console.log("PropertyNFT deployed: ", nft.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ test/index.ts
@@ -0,0 +1,85 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+import { ethers } from "hardhat";
+import { Marketplace, PropertyNFT } from "../typechain";
+
+let marketplace: Marketplace;
+let nft: PropertyNFT;
+let owner: SignerWithAddress;
+let seller: SignerWithAddress;
+let buyer: SignerWithAddress;
+
+describe("Marketplace", function () {
+  beforeEach(async function () {
+    [owner, seller, buyer] = await ethers.getSigners();
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    marketplace = (await Marketplace.connect(owner).deploy(
+      "0xdf3e18d64bc6a983f673ab319ccae4f1a57c7097"
+    )) as Marketplace;
+    await marketplace.deployed();
+
+    const PropertyNFT = await ethers.getContractFactory("PropertyNFT");
+    nft = (await PropertyNFT.connect(owner).deploy(marketplace.address)) as PropertyNFT;
+    await nft.deployed();
+  });
+
+  it("Should create a new listing and return", async function () {
+    let tx = await nft.mint("jkhgkjhgkjhgj");
+    await tx.wait();
+
+    tx = await marketplace.createListing(
+      nft.address,
+      1,
+      ethers.utils.parseUnits("1", "ether")
+    );
+
+    await tx.wait();
+
+    const allListings = await marketplace.getAllListings();
+    expect(allListings.length).to.be.equal(1);
+
+    const specificListing = await marketplace.getSpecificListing(1);
+    expect(specificListing.listingId).to.be.equal(1);
+  });
+
+  it("Should transfer nft to buyer after the sale and return nfts for owner and seller", async function () {
+    let tx = await nft.connect(seller).mint("token1");
+    await tx.wait();
+
+    tx = await nft.connect(seller).mint("token2");
+    await tx.wait();
+
+    tx = await marketplace
+      .connect(seller)
+      .createListing(nft.address, 1, ethers.utils.parseUnits("1", "ether"));
+    await tx.wait();
+
+    const nftsCreated = await marketplace.connect(seller).getListingsCreated();
+    expect(nftsCreated.length).to.be.equal(1);
+
+    tx = await marketplace
+      .connect(buyer)
+      .buy(1, { value: ethers.utils.parseUnits("1.03", "ether") });
+
+    await tx.wait();
+
+    tx = await marketplace
+    .connect(seller)
+    .createListing(nft.address, 2, ethers.utils.parseUnits("1", "ether"));
+  await tx.wait();
+
+    const myNfts = await marketplace.connect(buyer).getAllListings();
+    console.log(myNfts);
+/*     expect(myNfts.length).to.be.equal(1);
+
+    let balance = await nft.balanceOf(buyer.address);
+    expect(balance).to.be.equal(BigNumber.from(1));
+
+    balance = await nft.balanceOf(marketplace.address);
+    expect(balance).to.be.equal(BigNumber.from(0));
+
+    balance = await nft.balanceOf(seller.address);
+    expect(balance).to.be.equal(BigNumber.from(0)); */
+  });
+});
